01da89c53d9249de73020c25ee35009581b23ee1
Chester Leung <chester.leung@berkeley.edu>
2020-01-30 18:00:19 -0800

    Put tmux back in

--- setup.sh
+++ setup.sh
@@ -45,20 +45,20 @@ cp git/gitconfig ~/.gitconfig
 # ---------------------------------------------------------------------
 
 # install tmux
-# if [ "$(uname)" == "Darwin" ]; then
-#     brew install tmux
-#     brew install reattach-to-user-namespace
-#     cp tmux/tmux-macos.conf ~/.tmux.conf
-# else
-#     sudo apt-get install tmux
-#     sudo apt-get install xsel
-#     cp tmux/tmux-linux.conf ~/.tmux.conf
-# fi
-# 
-# # download plugin manager for tmux
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# 
-# ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+if [ "$(uname)" == "Darwin" ]; then
+    brew install tmux
+    brew install reattach-to-user-namespace
+    cp tmux/tmux-macos.conf ~/.tmux.conf
+else
+    sudo apt-get install tmux
+    sudo apt-get install xsel
+    cp tmux/tmux-linux.conf ~/.tmux.conf
+fi
+
+# download plugin manager for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
 # 
 # # ---------------------------------------------------------------------
 # # Tex
